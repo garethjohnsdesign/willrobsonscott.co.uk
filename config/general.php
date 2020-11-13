542556d37d1aa3701eb834b343e86b9b806fd162
Gareth Johns <garethjohns@Gareths-iMac.local>
2020-11-13 18:21:16 +0000

    Cache Bust

--- config/general.php
+++ config/general.php
@@ -48,7 +48,7 @@ return [
         'siteUrl'             => getenv('SITE_URL') ?: '@web',
 
         // Set the environmental variables
-        'staticAssetsVersion' => 8,
+        'staticAssetsVersion' => 9,
         
     ],
 
