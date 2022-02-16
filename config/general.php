22e42b18fe46c44f334b522c829bb6ca6f1ac022
Gareth Johns <info@garethjohnsdesign.com>
2022-02-16 10:35:50 +0000

    Update Static

--- config/general.php
+++ config/general.php
@@ -48,7 +48,7 @@ return [
         'siteUrl'             => getenv('SITE_URL') ?: '@web',
 
         // Set the environmental variables
-        'staticAssetsVersion' => 10,
+        'staticAssetsVersion' => 11,
         
     ],
 
